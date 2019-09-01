356c41b33b3090ad6d7676819196a385e62a80c7
TheStrix <parthbhatia98@gmail.com>
2019-09-01 22:07:54 +0530

    base: Add Circle battery style from PA [1/2]
    Extracted from https://github.com/AOSPA/android_frameworks_base/commit/0b09efa6399e0074180f310b3354f0a88dc51600 and added as an alternative to circle battery by Blacksuan19 & SKULSHADY

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -566,7 +566,9 @@ public class BatteryMeterView extends LinearLayout implements
     }
 
     private boolean isCircleBattery() {
-        return getMeterStyle() == BatteryMeterDrawableBase.BATTERY_STYLE_CIRCLE
+        return isBigCircleBattery()
+                || getMeterStyle() == BatteryMeterDrawableBase.BATTERY_STYLE_CIRCLE
+                || getMeterStyle() == BatteryMeterDrawableBase.BATTERY_STYLE_PA_CIRCLE
                 || getMeterStyle() == BatteryMeterDrawableBase.BATTERY_STYLE_DOTTED_CIRCLE;
     }
 
